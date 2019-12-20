70feda34d55fef5df7c5d3a4375d709ae2c3592d
danntastico <nicolasdpf@gmail.com>
2019-12-20 11:52:54 -0500

    inkwell finished

--- lib/button_purple.dart
+++ lib/button_purple.dart
@@ -11,7 +11,7 @@ class ButtonPurple extends StatelessWidget{
     return InkWell(
       onTap: (){
         Scaffold.of(context).showSnackBar(
-          SnackBar(
+            SnackBar(
               content: Text("Navegando")
           )
         );
