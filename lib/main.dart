19e2259124695e37830f5fe2a1bb5cec73b15ea5
danntastico <nicolasdpf@gmail.com>
2019-12-20 10:30:36 -0500

    Reduccion opacidad barra lateral

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:platzi_trips_app/description_place.dart';
 import 'package:platzi_trips_app/review_list.dart';
 import 'description_place.dart';
 import 'gradient_back.dart';
+import 'package:flutter/services.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,6 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.dark.copyWith(
+          statusBarColor: Color.fromRGBO(0, 0, 0, 0.0)
+        )
+    );
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
